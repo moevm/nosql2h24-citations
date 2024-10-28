c1dcc3d274053b6ca11e32453328148e91f90b42
Tatyana Yarusova <tata_yarusova@mail.ru>
2024-10-28 23:29:55 +0300

    add header

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import '../css/Header.css';
+
+const Header = () => {
+    return (
+        <header className="header">
+            <div className="logo">
+                <img src="./logo.svg" alt="Цитатник" />
+            </div>
+            <nav className="nav-menu">
+                <a href="#">Главная</a>
+                <a href="#">Авторы</a>
+                <a href="#">Произведения</a>
+                <a href="#">Герои</a>
+                <a href="#">Статистика</a>
+            </nav>
+        </header>
+    );
+};
+
+export default Header;
