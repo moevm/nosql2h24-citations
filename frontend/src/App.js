3d59c5c547b88a74057acd7d70168da03e2206e3
Tatyana Yarusova <tata_yarusova@mail.ru>
2024-10-29 17:05:31 +0300

    add search bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,19 @@
 import './App.css';
 import Sidebar from "./components/Sidebar";
 import Header from "./components/Header";
+import SearchBar from "./components/SearchBar";
 
 function App() {
   return (
       <div className="App">
           <Header/>
           <div className="main-content">
-              <Sidebar/>
+              <div className="sidebar">
+                  <Sidebar/>
+              </div>
+              <div className="content">
+                  <SearchBar/>
+              </div>
           </div>
       </div>
   );
