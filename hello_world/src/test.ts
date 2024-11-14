42a964d861ae4b65877905e4e682339fe88ab68c
VovaBas04 <vladimirbasyrov7@gmail.com>
2024-11-14 16:34:25 +0300

    delete hello world

--- hello_world/src/test.ts
+++
@@ -1,9 +0,0 @@
-export default async function test(client) {
-	const db = client.db(process.env.MONGO_INITDB_DATABASE)
-	const collection = db.collection("test")
-	const result = await collection.insertOne({"test" : "Hello World"})
-	console.log("Данные вставлены", result)
-	const testData = await collection.find().toArray()
-	console.log(testData)
-	console.log("Вставленные данные")		
-}
