fdb002223c1a061117768692379778a7fe8e24d4
Александр Маркуш <markush2004@mail.ru>
2024-11-15 18:49:22 +0300

    server: add books handler

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,6 +4,7 @@ import cors from 'cors';
 import { connectToDatabase } from './config/database';
 import quotesRoutes from './routes/quotesRoutes';
 import filtersRoutes from './routes/filtersRoutes';
+import booksRoutes from "./routes/booksRoutes";
 
 dotenv.config();
 
@@ -21,6 +22,7 @@ connectToDatabase();
 
 app.use('/quotes', quotesRoutes);
 app.use('/filters', filtersRoutes);
+app.use('/books', booksRoutes);
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
