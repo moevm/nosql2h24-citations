0bb0bcc74d00062815d4559472e16f69548d2836
Tatyana Yarusova <tata_yarusova@mail.ru>
2024-11-16 21:47:25 +0300

    add search book page

--- frontend/src/routers.js
+++ frontend/src/routers.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import MainPage from "./pages/MainPage";
+import SearchBookPage from "./pages/SearchBookPage";
 
 const AppRoutes = () => {
     return (
         <Routes>
             <Route path="/" element={<MainPage />} />
+            <Route path="/search_book" element={<SearchBookPage />} />
         </Routes>
     );
 };
